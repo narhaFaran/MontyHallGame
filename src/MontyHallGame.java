2bb4ba973e54ddc04d88f5c7b36d75b58d7ca2b4
narhaF <farhank9@nycstudents.net>
2023-11-08 13:18:01 -0500

    finished game

--- src/MontyHallGame.java
+++ src/MontyHallGame.java
@@ -2,12 +2,49 @@ import java.util.Scanner;
 public class MontyHallGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        MontyHallBot m = new MontyHallBot(1,2,3);
-        System.out.println(m.goat1);
-        System.out.println(m.goat2);
-        System.out.println(m.car);
-        int num = scanner.nextInt();
-        System.out.println(m.revealGoat(num));
+        MontyHallBot m = new MontyHallBot();
+        m.assignDoors();
+
+        System.out.println("Welcome to the Monty Hall Game!\n");
+        System.out.println("There are two goats and a car each behind doors 1, 2, and 3.");
+        System.out.println("Guess which door the car is in. 1, 2, or 3: ");
+        int guess = scanner.nextInt();
+        int oneGoat = m.revealGoat(guess);
+        System.out.println("There is a goat behind door " + oneGoat);
+        int hidden = 0;
+        if (guess == 1) {
+            if(oneGoat == 2) {
+                hidden = 3;
+            } else if (oneGoat == 3) {
+                hidden = 2;
+            }
+        } else if (guess == 2) {
+            if (oneGoat == 1) {
+                hidden = 3;
+            } else if (oneGoat == 3){
+                hidden = 1;
+            }
+        } else {
+            if (oneGoat == 1) {
+                hidden = 2;
+            } else {
+                hidden = 1;
+            }
+        }
+
+        System.out.println("Switch your guess? 0 for no, 1 for yes: ");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Guess switched from " + guess + " to " + hidden);
+            guess = hidden;
+        }
+        System.out.println("The car is behind door " + m.car + ". ");
+        if (guess == m.car) {
+            System.out.print("You won a car!");
+        } else {
+            System.out.print("You won a goat!");
+        }
 
     }
 
@@ -22,7 +59,7 @@ public class MontyHallGame {
         }
 
 
-        public void assignCards() {
+        public void assignDoors() {
             int num = (int)(Math.random()*3+1);
             this.car = num;
             if (num == 1) {
@@ -37,51 +74,46 @@ public class MontyHallGame {
             }
         }
 
-        public String revealGoat(int c) {
+        public int revealGoat(int c) {
             int num;
             if (c == 1) {
                 if (c == car) {
                     num = (int)(Math.random()*2);
                     if (num == 0) {
-                        return "There is a goat behind door " + goat1;
+                        return goat1;
                     } else {
-                        return "There is a goat behind door " + goat2;
+                        return goat2;
                     }
                 } else {
-                    if(car == 2) {
-                        return "There is a goat behind door " + goat2;
-                    } else {
-                        return "there is a goat behind door " + goat2;
-                    }
+                    return goat2;
                 }
             } else if (c == 2) {
                 if (c == car) {
                     num = (int)(Math.random()*2);
                     if (num == 1) {
-                        return "There is a goat behind door " + goat1;
+                        return goat1;
                     } else {
-                        return "There is a goat behind door " + goat2;
+                        return goat2;
                     }
                 } else {
                     if(car == 1) {
-                        return "There is a goat behind door " + goat2;
+                        return goat2;
                     } else {
-                        return "There is a goat behind door " + goat1;
+                        return goat1;
                     }
                 }
             } else  {
                 if (c == car) {
                     num = (int)(Math.random()*2);
                     if (num == 1) {
-                        return "There is a goat behind door " + goat1;
+                        return goat1;
                     } else {
-                        return "There is a goat behind door " + goat2;
+                        return goat2;
                     }
                 } else {
-                    return "There is a goat behind door " + goat1;
+                    return goat1;
                 }
             }
         }
-
     }
 }
